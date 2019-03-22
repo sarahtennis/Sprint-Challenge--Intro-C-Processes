9d0876cbae7c6146a46a23f61c93d054d04e17c8
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-03-22 13:49:28 -0400

    attempt at reformatting output

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -33,7 +33,7 @@ int main(int argc, char **argv)
   }
 
   // Repeatly read and print entries
-  int longest_size = 5;
+  // int longest_size = 5;
 
   while ((dp = readdir(dir)) != NULL)
   {
@@ -44,7 +44,14 @@ int main(int argc, char **argv)
     snprintf(path, 256, "%s/%s", directory, dp->d_name);
     stat(path, &get_size);
 
-    printf("%*lli %s\n", longest_size, get_size.st_size, dp->d_name);
+    // int size_chars = snprintf(NULL, 0, "%lli", get_size.st_size);
+    // if (size_chars > longest_size)
+    // {
+    //   longest_size = size_chars;
+    //   printf("new size: %d\n", longest_size);
+    // }
+
+    printf("%*lli %s\n", 10, get_size.st_size, dp->d_name);
   }
 
   // Close directory
