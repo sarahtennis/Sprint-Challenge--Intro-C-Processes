869c6d3d24113bc3819c3e31644c5a3c7969892c
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-03-22 12:03:23 -0400

    completes ANSWERS.md

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -2,7 +2,9 @@
 #include <dirent.h>
 
 /**
- * Main
+ * Prints out a directory listing for the directory the user specifies on the command line,
+ * including both file names and sizes. If the user does not specify a directory, print out
+ * the contents of the current directory.
  */
 int main(int argc, char **argv)
 {
@@ -16,3 +18,11 @@ int main(int argc, char **argv)
 
   return 0;
 }
+
+/* EXAMPLE
+  $ ./lsls
+      224  .
+      992  ..
+      1722  lsls.c
+      8952  lsls
+*/
